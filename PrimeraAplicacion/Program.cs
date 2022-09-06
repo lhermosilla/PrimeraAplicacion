6bbb9e5f233b6cc8b4348bce0c64b1a9f9771f7c
Luis Hermosilla <lhermosilla@technologyhf.com>
2022-09-05 22:43:42 -0400

    Initial commit

---
+++ PrimeraAplicacion/Program.cs
@@ -0,0 +1,15 @@
+﻿//System es un namespace que trae c#, ej la clase Console, es de c#
+using System;
+
+//Sirve para poder nombrar clases con el mismo nombre EJEMPLO PERSONA, namespace cumple función de tipo carpeta, en donde los archivos adentro con el mismo nombre dejan de estar en conflicto
+
+namespace PrimeraAplicacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a C#");   //Esto imprime un mensaje en consola
+        }
+    }
+}
